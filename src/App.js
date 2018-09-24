cce4a68d059f50e335e5dc008e3a41f0eec95b37
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-09-24 16:09:56 +0200

    add busy-sdk

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { createClient } from "@sekhmetdev/busy-sdk";
+import logo from "./logo.svg";
+import "./App.css";
+
+const client = createClient(window.parent);
+
+let id = 0;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rooms: []
+    };
+  }
+
+  handleRefreshClick = () => {
+    client.call(id++, "get_rooms", []).then(rooms =>
+      this.setState({
+        rooms
+      })
+    );
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +31,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Rooms</h2>
+        <button onClick={this.handleRefreshClick}>Refresh</button>
+        <ul>
+          {this.state.rooms.map(room => (
+            <li key={room.id}>{room.displayName}</li>
+          ))}
+        </ul>
       </div>
     );
   }
