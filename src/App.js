8de69e87bd1d133dcfcdc3a911f8252c651b63c5
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-10-03 15:37:05 +0200

    feat: handle platform errors

--- src/App.js
+++ src/App.js
@@ -10,19 +10,29 @@ class App extends Component {
     super(props);
 
     this.state = {
-      rooms: []
+      rooms: [],
+      error: null
     };
   }
 
   handleRefreshClick = () => {
-    client.call("get_rooms", []).then(rooms =>
-      this.setState({
-        rooms
-      })
-    );
+    client
+      .call("get_rooms", [])
+      .then(rooms =>
+        this.setState({
+          rooms
+        })
+      )
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
   };
 
   render() {
+    const { error } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
@@ -30,7 +40,10 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <h2>Rooms</h2>
-        <button onClick={this.handleRefreshClick}>Refresh</button>
+        {error ? error : null}
+        <button onClick={this.handleRefreshClick}>
+          {error ? "Retry" : "Load"}
+        </button>
         <ul>
           {this.state.rooms.map(room => (
             <li key={room.id}>{room.displayName}</li>
